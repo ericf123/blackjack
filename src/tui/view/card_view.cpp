eced7e2dea5b57f5fe61224199db3a28e3b78b35
Eric Frankel <7970518+ericf123@users.noreply.github.com>
2021-12-27 05:40:47 -0500

    update `CardView` to display card suit/color

--- src/tui/view/card_view.cpp
+++ src/tui/view/card_view.cpp
@@ -16,18 +16,35 @@ void CardView::draw() {
   wclear(window.get()); // otherwise 10 leaves its 0 when you go back to one
                         // char card
 
-  std::string displayStr{ "" };
   auto color_pair = COLOR_PAIR(bjcolor::PAIR_CARD_DOWN);
 
+  auto suit = Suit::Hearts;
   if (card) {
-    displayStr = getDisplayStr(card.value());
-    color_pair = COLOR_PAIR(bjcolor::PAIR_CARD_BLACK);
+    suit = std::visit([](const auto& c) -> Suit { return c.suit; }, card.value());
+    if (suit == Suit::Hearts || suit == Suit::Diamonds) {
+      color_pair = COLOR_PAIR(bjcolor::PAIR_CARD_RED);
+    } else {
+      color_pair = COLOR_PAIR(bjcolor::PAIR_CARD_BLACK);
+    }
   }
 
   wattron(window.get(), color_pair);
   wbkgd(window.get(), color_pair);
   box(window.get(), 0, 0);
-  mvwprintw(window.get(), height / 2, width / 2, "%s", displayStr.c_str());
+
+  if (card) {
+    std::ostringstream rankDisplay;
+    rankDisplay << std::visit([](const auto& c) -> Rank { return c.rank; }, card.value());
+
+    std::ostringstream suitDisplay;
+    suitDisplay << suit;
+
+    wattron(window.get(), A_BOLD);
+    mvwprintw(window.get(), 1, 1, "%s", suitDisplay.str().c_str());
+    mvwprintw(window.get(), height - 2, width - 2, "%s", suitDisplay.str().c_str());
+    mvwprintw(window.get(), height / 2, width / 2, "%s", rankDisplay.str().c_str());
+    wattroff(window.get(), A_BOLD);
+  }
 }
 
 void CardView::update() { draw(); }
